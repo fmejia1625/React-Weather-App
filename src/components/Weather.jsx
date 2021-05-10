ff432947cdc10991920e1757a628df52c8ee3a54
Francisco Mejia <frankie@Franciscos-MacBook-Pro.local>
2021-05-10 05:53:23 -0400

    added function that retrieves API info

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,10 +5,27 @@ import './weatherstyle.css'
 // using functional components over class components due to ease of use. 
 // Here we import useState from react and then set const [form, setForm] to useState
 function Weather() {
+  // create a variable to will hold api weather key
+const weatherKey = '5162a3caf9bbd39fc2394c2ab186c5d0';
+
+
+
 const [form,setForm] = useState({
   city:"", 
   country: ""
-})
+});
+
+// using asynchronous function in order to fetch data from weather API. If the input is empty, alert the user. else, create variable data and use await fetch in order to retrieve data of city from API. Use template strings here (like pokemon API) 
+// use .then to create a promise that will get response (res) and log it in JSON format. 
+async function weatherData(e) {
+
+  e.preventDefault();
+  if(form.city === "") {
+    alert("Please enter a city");
+  } else {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&appid=${weatherKey}`).then(res => console.log(res.json()))
+  }
+}
 
 // function that will handle changes
 const handleChange = (e) => {
@@ -39,7 +56,8 @@ const handleChange = (e) => {
         {/* add spaces using  nonbreaking space (HTML entity)*/}
         &nbsp; &nbsp; 
         <input type="text" name="country" placeholder="country" onChange={e => handleChange(e)}/>
-        <button className="getWeather">Submit</button>
+        {/* evoke onClick function, pass event to weatherData */}
+        <button className="getWeather" onClick={e => weatherData(e)}>Submit</button>
       </form>
     </div>
   );
