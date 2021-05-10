e3e87beff27894ae58a934737d97b538877aa241
Francisco Mejia <frankie@Franciscos-MacBook-Pro.local>
2021-05-10 05:35:32 -0400

    second commit added functionality to both input fields

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './weatherstyle.css'
 
+
+// using functional components over class components due to ease of use. 
+// Here we import useState from react and then set const [form, setForm] to useState
 function Weather() {
+const [form,setForm] = useState({
+  city:"", 
+  country: ""
+})
+
+// function that will handle changes
+const handleChange = (e) => {
+  let name = e.target.name; 
+  let value = e.target.value; 
+// if name is city,  set value of city in the form state, use spread operator to set form to form and city:value
+  if(name === "city") {
+    setForm({...form, city:value})
+  }
+// The smae logic is applied to the country:value
+  if(name === "country") {
+    setForm({...form, country:value})
+  }
+  // print out the result using form.city and form.country
+  console.log(form.city, form.country)
+};
+
   return (
     <div className = "weather">
       <span className="title">
@@ -10,8 +34,11 @@ function Weather() {
       <br/>
 
       <form >
-        <input type="text" name="city" placeholder="city"/>
-        <input type="text" name="country" placeholder="country"/>
+        {/* invoke handleChange function here while passing the event with arrow function */}
+        <input type="text" name="city" placeholder="city" onChange={e => handleChange(e)}/>
+        {/* add spaces using  nonbreaking space (HTML entity)*/}
+        &nbsp; &nbsp; 
+        <input type="text" name="country" placeholder="country" onChange={e => handleChange(e)}/>
         <button className="getWeather">Submit</button>
       </form>
     </div>
