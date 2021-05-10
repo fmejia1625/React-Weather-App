562d30ec7f7b1a0d06097fa52146c04355f885ee
Francisco Mejia <frankie@Franciscos-MacBook-Pro.local>
2021-05-10 06:41:14 -0400

    converted temp from Kelvin to Farenheit

--- src/components/ShowWeather.jsx
+++ src/components/ShowWeather.jsx
@@ -3,12 +3,35 @@
 import React from 'react'
 
 function ShowWeather(props) {
-  console.log("props", props);
+  // console.log("props", props);
+  const {data} = props; 
+  console.log(data); 
+
+
   return (
-    <div>
+    <div className= "displayweather">
+      <div className="maincard">
+        <span className="cardtitle">
+          {data.name}, {data.sys.country}. Weather
+        </span>
+        &nbsp; &nbsp; 
+        <span className="cardsubtitle">
+          As of {new Date().toLocaleTimeString()}
+        </span>
+        <h1>
+          {Math.floor(((data.main.temp - 273) * 1.8) + 32)}
+          <sup>o</sup>
+        </h1>
+      </div>
       
     </div>
   )
 }
 
 export default ShowWeather
+
+// Create a div with class name of display weather. Have a main card div that will contain the card title which spands the data.name and data.sys.country, thereby displaying the weather. 
+// In the second psan labeled cardsubtitle, use new Date () to display date, and then .toLocaleTimeString method to display the current time. 
+// Create an h1 and within pass the props of {data.main.temp} in order to show the temperature. 
+// Googled how to convert kelvin to farenheit since data is displayed only in kelvin using math.floor. 
+// used super tag to enclose o for temp symbol
